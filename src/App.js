12819fd913fc4a27316c6280b6d9c03db86fe2c3
solyDenis <solydenis38@gmail.com>
2023-03-01 12:03:43 -0500

    install daisyui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-amber-700">hello</h1>
+        <div className="btn-group">
+            <button className="btn btn-active">Button</button>
+            <button className="btn">Button</button>
+            <button className="btn">Button</button>
+        </div>
     </div>
   );
 }
